d1c6dab73373f7e1f97a11acc1f2673dda8f3bc0
young <youngyang0820@gmail.com>
2018-10-16 00:40:46 -0700

    algo

--- algorithm/linear_regression_algorithm.py
+++ algorithm/linear_regression_algorithm.py
@@ -53,69 +53,21 @@ class LinearRegression(AlgorithmBase):
     def cleanup(self):
         pass
 
-    def gradient_descent(self, designMatrix, targetMatrix):
-        count = 0
-        w_old = np.zeros((3, 1))
-        w_new = np.zeros((3, 1))
-        E_old = 0
-        E_new = 0
-        delta_E = np.zeros((len(designMatrix), 3))
-        learning_rate = 0.001
-        # tolerance = 1e-5
-        while True:
-            w_old = w_new
-
-            for i in range(len(designMatrix)):
-                delta_E[i,:] = delta_E[i,:] + (targetMatrix[i][0] - np.dot(np.matrix(designMatrix[i,:]), np.matrix(w_old))) * designMatrix[i,:]
-            print (delta_E[i, :])
-            
-            w_new = w_old + learning_rate * np.matrix(delta_E[i, :] / (len(designMatrix))).T
-            E_old = E_new
-
-            for i in range(len(designMatrix)):
-                E_new = E_new + (targetMatrix[i][0] - np.dot(np.matrix(designMatrix[i, :]), np.matrix(w_new))) ** 2
-                E_new = E_new / 2
-
-            if E_new > E_old:
-                learning_rate = learning_rate / 2
-
-            count = count + 1
-            # print("E_new", E_new, "E_old", E_old)
-            if count % 20 == 0:
-                # print(" ".join[count, "iterations so far..."])
-                print(str(count), " iterations so far...")
-
-        # Test if restricting iterations affects the quality
-            if count == 50:
-                break
-
-            # Comparing E_new == E_old is tricky because of precision.
-            #if np.isclose(E_new, E_old)[0]:
-            #print("Escaped loop after", str(count), "iterations.")
-            #break
-
-            return w_new
- """  print(X)
-        print(y)
+    def gradient_descent(self, X, y):
         w = np.zeros((X.shape[1], 1))
         b = 0
 
         for epoch in range(self._epochs):
             pred = X.dot(w) + b
             print(pred)
-            print(y)
             dloss = pred - y[:, None]
-            print("dloss")
-#            print(dloss)
             dw = np.dot(X.T, dloss)/X.shape[0]
-            print("dw")
-            print(dw)
             db = np.sum(dloss)/X.shape[0]
             w -= self._lr * dw
             b -= self._lr * db
             print(w)
         return np.around(w, decimals=4), np.around(b, decimals=4)
-"""
+
     def send(self, down_addr):
         self._logger.info('LinearRegression send start...')
         if self._local is None:
