55043fd63cabc99a54d8fec1feb1ee1001c3bbee
young <youngyang0820@gmail.com>
2018-12-11 17:59:07 -0800

    19000

--- data_collector/mnist_data_collector.py
+++ data_collector/mnist_data_collector.py
@@ -4,7 +4,7 @@ from .data_collector_base import DataCollectorBase
 from utils import bluetootch_utils
 import numpy as np
 
-DATA_POINTS = 20000
+DATA_POINTS = 19000
 
 class MNISTDataCollector(DataCollectorBase):
     """Linear Regression Data Collector
